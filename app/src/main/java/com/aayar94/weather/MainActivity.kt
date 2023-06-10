a27a8f23c53d31d9466b76c17bdde5c209821bf9
Adem Ayar <79112917+AAyar94@users.noreply.github.com>
2023-06-10 17:52:19 +0300

    init commit

---
+++ app/src/main/java/com/aayar94/weather/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.aayar94.weather
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.aayar94.weather.ui.theme.WeatherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherTheme {
+                WeatherApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun WeatherApp() {
+    Text(text = "Hello Weather App")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppPreview() {
+    WeatherTheme {
+        WeatherApp()
+    }
+}
