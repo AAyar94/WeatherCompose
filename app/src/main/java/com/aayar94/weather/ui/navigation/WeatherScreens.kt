963af4446d0c1558fd46b184e28b3d806b9837b6
Adem Ayar <a.ayar94@gmail.com>
2023-11-07 18:28:23 +0300

    New Screen added to enum class

--- app/src/main/java/com/aayar94/weather/ui/navigation/WeatherScreens.kt
+++ app/src/main/java/com/aayar94/weather/ui/navigation/WeatherScreens.kt
@@ -1,12 +1,26 @@
 package com.aayar94.weather.ui.navigation
 
 enum class WeatherScreens {
+    Onboarding,
+    Permission,
     HomeScreen,
+    Humidity,
+    Visibility,
+    Wind,
+    UltraViole,
+    Sun,
     DetailScreen;
 
     companion object {
         fun fromRoute(route: String?): WeatherScreens = when (route?.substringBefore("/")) {
+            Onboarding.name -> Onboarding
+            Permission.name -> Permission
             HomeScreen.name -> HomeScreen
+            Humidity.name -> Humidity
+            Visibility.name -> Visibility
+            Wind.name -> Wind
+            UltraViole.name -> UltraViole
+            Sun.name -> Sun
             DetailScreen.name -> DetailScreen
             null -> HomeScreen
             else -> throw IllegalArgumentException("Route $route is not recognized")
