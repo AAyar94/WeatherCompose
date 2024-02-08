bbfdb1998c06eee8618035e2a9fadc977d819f95
Adem Ayar <ademayar09@gmail.com>
2024-02-08 14:40:54 +0300

    fix:Base request url

--- app/src/main/java/com/aayar94/weather/core/util/Constant.kt
+++ app/src/main/java/com/aayar94/weather/core/util/Constant.kt
@@ -1,5 +1,5 @@
 package com.aayar94.weather.core.util
 
 object Constant {
-    const val BASE_URL = "tps://api.open-meteo.com/v1/forecast?"
+    const val BASE_URL = "https://api.open-meteo.com/v1/"
 }
--- app/src/main/java/com/aayar94/weather/data/remote/WeatherAPI.kt
+++ app/src/main/java/com/aayar94/weather/data/remote/WeatherAPI.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherAPI {
-    @GET("&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,weather_code,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,rain,showers,weather_code,pressure_msl,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max&timezone=Europe%2FMoscow")
+    @GET("forecast?current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,weather_code,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,rain,showers,weather_code,pressure_msl,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max&timezone=Europe%2FMoscow")
     fun getWeather(
         @Query("latitude") latitude: Double,
         @Query("longutide") longitude: Double
